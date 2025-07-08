4f18f1a288bb85c795db04cacee46c749bd70999
vrundanawal12 <vrunda.nawal@cognizant.com>
2025-07-08 14:53:28 +0100

    basic route setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,23 @@
 import express from 'express';
 
 const app = express();
+
+app.get('/api/notes', (req, res) => {
+  res.send('You got 100 notes!');
+});
+
+app.post('/api/notes', (req, res) => {
+  res.status(201).json({ message: 'Note created successfully!' });
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  res.status(200).json({ message: `Note updated successfully!` });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  res.status(200).json({ message: `Note  deleted successfully!` });
+});
+
 app.listen(5001, () => {
   console.log('Server is running on PORT: 5001');
 });
