051e8939347d0d93d0be2cc1eb79ee16f0e6aff3
vrundanawal12 <vrunda.nawal@cognizant.com>
2025-07-09 15:17:55 +0100

    if we are not using any parameter then update it in using '_' and dispaly the notes with latest first approch

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,8 +1,8 @@
 import Note from '../models/Note.js';
 
-export const getAllNotes = async (req, res) => {
+export const getAllNotes = async (_, res) => {
   try {
-    const notes = await Note.find({});
+    const notes = await Note.find().sort({ createdAt: -1 }); // newest notes first
     res.status(200).json(notes);
   } catch (error) {
     console.error('Error fetching notes:', error);
