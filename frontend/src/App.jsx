99eb2d52da34af9320039884cd7f7d23376a2113
vrundanawal12 <vrunda.nawal@cognizant.com>
2025-07-10 17:48:30 +0100

    daisyui setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,13 @@ import { Route, Routes } from 'react-router';
 import Homepage from './pages/Homepage';
 import NoteDetailPage from './pages/NoteDetailPage';
 import CreatePage from './pages/CreatePage';
-import toast from 'react-hot-toast';
 
 const App = () => {
   return (
-    <div>
-      <button
-        onClick={() => toast.success('congrates')}
-        className="text-red-500 p-4"
-      >
-        Click Me
-      </button>
+    <div data-theme="forest">
+      <button className="btn btn-primary">Click me</button>
       <Routes>
         <Route path="/" element={<Homepage />} />
-
         <Route path="/create" element={<CreatePage />} />
         <Route path="/note/:id" element={<NoteDetailPage />} />
         {/* Add more routes as needed */}
