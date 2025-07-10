43e1ba1010bd7432378fbf4bf2e7812b69485aa7
vrundanawal12 <vrunda.nawal@cognizant.com>
2025-07-10 14:01:31 +0100

    Create a pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,19 @@
+import { Route, Routes } from 'react-router';
+import Homepage from './pages/Homepage';
+import NoteDetailPage from './pages/NoteDetailPage';
+import CreatePage from './pages/CreatePage';
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
