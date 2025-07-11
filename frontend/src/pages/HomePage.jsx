d65675a89fb5bcafe7153a3000074feb9386ca54
vrundanawal12 <vrunda.nawal@cognizant.com>
2025-07-11 16:17:33 +0100

    updated the ratelimit to get the rateLimit page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,39 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Navbar from '../components/Navbar';
 import RateLimitedUI from '../components/RateLimitedUI';
+import toast from 'react-hot-toast';
 
 const HomePage = () => {
-  const [isRateLimited, setIsRateLimited] = useState(true);
+  const [isRateLimited, setIsRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const response = await axios.get('http://localhost:5001/api/notes');
+        setNotes(response.data);
+        setIsRateLimited(false);
+      } catch (error) {
+        if (error.response && error.response?.status === 429) {
+          setIsRateLimited(true);
+          toast.error(
+            'You have exceeded the rate limit. Please try again later.'
+          );
+        } else {
+          setIsRateLimited(false);
+          toast.error('Failed to fetch notes. Please try again later.');
+        }
+      } finally {
+        // Additional cleanup or final actions can be performed here if needed
+        setLoading(false);
+      }
+    };
+
+    fetchNotes();
+  }, []);
+
   return (
     <div className="min-h-screen">
       <Navbar />
