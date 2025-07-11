71c6b4ef5d36021d5c65f63178c872200e44c774
vrundanawal12 <vrunda.nawal@cognizant.com>
2025-07-11 17:56:10 +0100

    CreatePage Ui

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,7 +1,59 @@
-import React from 'react';
+import { ArrowLeftIcon } from 'lucide-react';
+import { useState } from 'react';
+import toast from 'react-hot-toast';
+import { Link, useNavigate } from 'react-router';
 
 const CreatePage = () => {
-  return <div>CreatePage</div>;
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {};
+
+  return (
+    <div className="min-h-screen bg-base-200">
+      <div className="container mx-auto px-4 py-8">
+        <div className="max-w-2xl mx-auto">
+          <Link to={'/'} className="btn btn-ghost mb-6">
+            <ArrowLeftIcon className="size-5" />
+            Back to Notes
+          </Link>
+
+          <div className="card bg-base-100">
+            <div className="card-body">
+              <h2 className="card-title text-2xl mb-4">Create New Note</h2>
+              <form onSubmit={handleSubmit}>
+                <div className="form-control mb-4">
+                  <label className="label">
+                    <span className="label-text">Title</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Note Title"
+                    className="input input-bordered"
+                  />
+                </div>
+
+                <div className="form-control mb-4">
+                  <label className="label">
+                    <span className="label-text">Content</span>
+                  </label>
+                  <textarea
+                    placeholder="Write your note here..."
+                    className="textarea textarea-bordered h-32"
+                  />
+                </div>
+
+                <div className="card-actions justify-end">
+                  <button type="submit" className="btn btn-primary"></button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePage;
