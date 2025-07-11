c95a020948950c43608c4b906e553f84beb39533
vrundanawal12 <vrunda.nawal@cognizant.com>
2025-07-11 15:02:06 +0100

    RateLimitPage UI and display on home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import Navbar from '../components/Navbar';
+import RateLimitedUI from '../components/RateLimitedUI';
 
 const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = useState(true);
   return (
     <div className="min-h-screen">
       <Navbar />
+      {isRateLimited && <RateLimitedUI />}
     </div>
   );
 };
