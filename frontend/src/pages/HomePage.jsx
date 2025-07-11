8679635eebf23c21442ffb184386fc9071c907b4
vrundanawal12 <vrunda.nawal@cognizant.com>
2025-07-11 16:56:13 +0100

    Display the notes on homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -38,6 +38,22 @@ const HomePage = () => {
     <div className="min-h-screen">
       <Navbar />
       {isRateLimited && <RateLimitedUI />}
+
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && (
+          <div className="text-center text-primary py-10">Loading notes...</div>
+        )}
+
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note) => (
+              <div key={note._id}>
+                {note.title} | {note.content}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
