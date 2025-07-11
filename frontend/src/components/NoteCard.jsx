ce4769b8f0c0a6582de5cf3f2b4dd63f6efe53a1
vrundanawal12 <vrunda.nawal@cognizant.com>
2025-07-11 17:06:45 +0100

    added the Link

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,9 +1,16 @@
+import { Link } from 'react-router';
 const NoteCard = ({ note }) => {
   return (
-    <div className="card-body">
-      <h3 className="card-title text-base-content">{note.title}</h3>
-      <p className="text-base-content/70 line-clamp-3">{note.content}</p>
-    </div>
+    <Link
+      to={`/note/${note._id}`}
+      className="card bg-base-100 hover:shadow-lg transition-all duration-200 
+    border-t-4 border-solid border-[#00FF9D]"
+    >
+      <div className="card-body">
+        <h3 className="card-title text-base-content">{note.title}</h3>
+        <p className="text-base-content/70 line-clamp-3">{note.content}</p>
+      </div>
+    </Link>
   );
 };
 
