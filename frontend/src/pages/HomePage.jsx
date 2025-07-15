e77fbc5e695e00128d8152936f21ee35db0b609f
vrundanawal12 <vrunda.nawal@cognizant.com>
2025-07-15 17:02:07 +0100

    if there is not a sigle note then show notenotefound component conditionally

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import RateLimitedUI from '../components/RateLimitedUI';
 import toast from 'react-hot-toast';
 import NoteCard from '../components/NoteCard';
 import api from '../lib/axios';
+import NotesNotFound from '../components/NotesNotFound';
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -45,7 +46,7 @@ const HomePage = () => {
           <div className="text-center text-primary py-10">Loading notes...</div>
         )}
 
-        {/* {notes.length === 0 && !isRateLimited && <NotesNotFound />} */}
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
 
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
